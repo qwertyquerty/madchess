8698850f5ff2b4f2115074624caed8a5da5e5596
madeline <qwertytrogi@gmail.com>
2023-05-02 06:18:38 -0700

    movetime fix

--- qchess/qchess.py
+++ qchess/qchess.py
@@ -590,9 +590,9 @@ if __name__ == "__main__":
 					binc = 0
 				
 				if board.turn:
-					allowed_movetime = min(wtime / 40 + winc, max(wtime / 2 - 1000, 0))
+					allowed_movetime = max(min(wtime / 40 + winc, max(wtime / 2 - 1000, 0)), 100)
 				else:
-					allowed_movetime = min(btime / 40 + binc, max(btime / 2 - 1000, 0))
+					allowed_movetime = max(min(btime / 40 + binc, max(btime / 2 - 1000, 0)), 100)
 
 			else:
 				allowed_movetime = None
